c6da51f1e06ef63147ff0bfdfcbc7ec14939693b
KarnellSchultz <KarnellSchultz@gmail.com>
2020-03-14 08:41:11 +0100

    updated dark theme func

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,25 +1,25 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const DashboardPage = ({ themeToggle }) => {
-  return (
-    <>
-      <nav>
-        <section>
-          <a href="/">Hola</a>
-          <a href="/">Hola</a>
-          <Link to="/" onClick={themeToggle}>
-            Dark/Light
-          </Link>
-        </section>
-      </nav>
-      <section>
-        <h1>Hola!</h1>
-        <p>This is the Dashboard.</p>
-        <Link to="posts" className="button">
-          View Posts
-        </Link>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<nav>
+				<section>
+					<a href="/">Hola</a>
+					<Link to="/" onClick={themeToggle}>
+						Dark/Light
+					</Link>
+				</section>
+			</nav>
+			<section>
+				<h1>Hola!</h1>
+				<p>This is the Dashboard.</p>
+				<Link to="posts" className="button">
+					View Posts
+				</Link>
+				<div id="wave"></div>
+			</section>
+		</>
+	);
 };
--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,34 +1,32 @@
-import * as actions from "../actions/postsActions";
-import { lightTheme, darkTheme } from "../theme";
+import * as actions from '../actions/postsActions';
 
 export const initialState = {
-  posts: [],
-  loading: false,
-  hadErrors: false,
-  theme: lightTheme
+	posts: [],
+	loading: false,
+	hadErrors: false,
 };
 
 export default function postsReducer(state = initialState, action) {
-  switch (action.type) {
-    case actions.GET_POSTS:
-      return { ...state, loading: true };
-    case actions.GET_POSTS_SUCCESS:
-      return {
-        ...state,
-        posts: action.payload,
-        loading: false,
-        hadErrors: false
-      };
-    case actions.GET_CMS_SUCCESS:
-      return {
-        ...state,
-        posts: action.payload,
-        loading: false,
-        hadErrors: false
-      };
-    case actions.GET_POSTS_FAILURE:
-      return { ...state, loading: false, hasErorrs: true };
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case actions.GET_POSTS:
+			return { ...state, loading: true };
+		case actions.GET_POSTS_SUCCESS:
+			return {
+				...state,
+				posts: action.payload,
+				loading: false,
+				hadErrors: false,
+			};
+		case actions.GET_CMS_SUCCESS:
+			return {
+				...state,
+				posts: action.payload,
+				loading: false,
+				hadErrors: false,
+			};
+		case actions.GET_POSTS_FAILURE:
+			return { ...state, loading: false, hasErorrs: true };
+		default:
+			return state;
+	}
 }
